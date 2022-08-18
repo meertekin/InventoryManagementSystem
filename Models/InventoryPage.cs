3026c1ee829cb1464b48f8bd466942641499f7ec
Mert Ekin <bmertekin@gmail.com>
2022-08-18 17:20:07 +0300

    Update InventoryPage.cs

--- Models/InventoryPage.cs
+++ Models/InventoryPage.cs
@@ -9,9 +9,14 @@ namespace InventoryManagementSystem.Models
         [Key]
 
         public int Id { get; set; }
+        [Required]
+
         public string Name { get; set; }
+
         public string Type { get; set; }
+
         public DateTime PurchaseDate { get; set; } = DateTime.Now;
+
         public double Price { get; set; }
 
 
