d7ec48783f9c758e6b269cc6898aa72255fd25c9
Mert Ekin <bmertekin@gmail.com>
2022-08-18 17:17:34 +0300

    inventoryPage --> variables for db

---
+++ Models/InventoryPage.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementSystem.Models
+{
+    public class InventoryPage
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime PurchaseDate { get; set; } = DateTime.Now;
+        public double Price { get; set; }
+
+
+    }
+}
+
